bdf2da2836f5e4f9d6137c966a06a1495ba5043f
Stanley Mesa <83571092+stanleymesa@users.noreply.github.com>
2022-03-10 15:44:05 +0700

    build config

--- app/src/main/java/com/example/gethub/api/ApiServices.kt
+++ app/src/main/java/com/example/gethub/api/ApiServices.kt
@@ -1,5 +1,6 @@
 package com.example.gethub.api
 
+import com.example.gethub.BuildConfig
 import com.example.gethub.responses.DetailResponse
 import com.example.gethub.responses.FollowersFollowingResponseItem
 import com.example.gethub.responses.SearchResponse
@@ -10,19 +11,20 @@ import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiServices {
+
     @GET("search/users")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getSearchUser(@Query("q") username: String) : Call<SearchResponse>
 
     @GET("users/{username}")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getDetailUser(@Path("username") username: String) : Call<DetailResponse>
 
     @GET("users/{username}/followers")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getFollowers(@Path("username") username: String) : Call<List<FollowersFollowingResponseItem>>
 
     @GET("users/{username}/following")
-    @Headers("Authorization: token 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")
+    @Headers("Authorization: token ${BuildConfig.GITHUB_TOKEN}")
     fun getFollowing(@Path("username") username: String) : Call<List<FollowersFollowingResponseItem>>
 }
